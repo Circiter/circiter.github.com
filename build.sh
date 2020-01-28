2b963b5550b3fd6218e10c623bedd03c6eb6360e
Circiter <xcirciter@gmail.com>
2020-01-28 23:55:12 +0600

    -

--- build.sh
+++ build.sh
@@ -2,45 +2,22 @@
 
 bundle install
 
-mkdir "$FOLDER"
+mkdir --parents "$FOLDER/.github/workflows"
+mv post_workflow "$FOLDER/.github/workflow/workflow.yml"
+
 bundle exec jekyll build --trace
 
-echo destination folder is "$FOLDER"
+echo Destination folder is "$FOLDER"
 cd "$FOLDER"
 echo > .nojekyll
 if [ -e index.html ]; then mv index.html _.html; fi
 echo 'under construction' > index.html
 cd ..
 
-set -e
-
-if [ -z "$ACCESS_TOKEN" ] && [ -z "$GITHUB_TOKEN" ]
-then
-  echo "You must provide the action with either a Personal Access Token or the GitHub Token secret in order to deploy."
-  exit 1
-fi
-
-if [ -z "$BRANCH" ]
-then
-  echo "You must provide the action with a branch name it should deploy to, for example gh-pages or docs."
-  exit 1
-fi
-
-if [ -z "$FOLDER" ]
-then
-  echo "You must provide the action with the folder name in the repository where your compiled page lives."
-  exit 1
-fi
-
-case "$FOLDER" in /*|./*)
-  echo "The deployment folder cannot be prefixed with '/' or './'. Instead reference the folder name directly."
-  exit 1
-esac
-
 # Installs Git and jq.
-apt-get update && \
-apt-get install -y git && \
-apt-get install -y jq && \
+apt-get update
+apt-get install -y git
+apt-get install -y jq
 
 # Gets the commit email/name if it exists in the push event payload.
 COMMIT_EMAIL=`jq '.pusher.email' ${GITHUB_EVENT_PATH}`
@@ -58,61 +35,25 @@ then
 fi
 
 # Directs the action to the the Github workspace.
-cd $GITHUB_WORKSPACE && \
+cd $GITHUB_WORKSPACE
 
 # Configures Git.
-git init && \
-git config --global user.email "${COMMIT_EMAIL}" && \
-git config --global user.name "${COMMIT_NAME}" && \
+git init
+git config --global user.email "${COMMIT_EMAIL}"
+git config --global user.name "${COMMIT_NAME}"
 
 ## Initializes the repository path using the access token.
-REPOSITORY_PATH="https://${ACCESS_TOKEN:-"x-access-token:$GITHUB_TOKEN"}@github.com/${GITHUB_REPOSITORY}.git" && \
-
-# Checks to see if the remote exists prior to deploying.
-# If the branch doesn't exist it gets created here as an orphan.
-if [ "$(git ls-remote --heads "$REPOSITORY_PATH" "$BRANCH" | wc -l)" -eq 0 ];
-then
-  echo "Creating remote branch ${BRANCH} as it doesn't exist..."
-  git checkout "${BASE_BRANCH:-master}" && \
-  git checkout --orphan $BRANCH && \
-  git rm -rf . && \
-  touch README.md && \
-  git add README.md && \
-  git commit -m "Initial ${BRANCH} commit" && \
-  git push $REPOSITORY_PATH $BRANCH
-fi
+REPOSITORY_PATH="https://${ACCESS_TOKEN:-"x-access-token:$GITHUB_TOKEN"}@github.com/${GITHUB_REPOSITORY}.git"
 
 # Checks out the base branch to begin the deploy process.
-git checkout "${BASE_BRANCH:-master}" && \
-
-# Builds the project if a build script is provided.
-echo "Running build scripts... $BUILD_SCRIPT" && \
-eval "$BUILD_SCRIPT" && \
+git checkout "${BASE_BRANCH:-master}"
 
 if [ "$CNAME" ]; then
-  echo "Generating a CNAME file in in the $FOLDER directory..."
   echo $CNAME > $FOLDER/CNAME
 fi
 
 # Commits the data to Github.
-echo "Deploying to GitHub..." && \
-git add -f $FOLDER && \
+git add -f $FOLDER
 
-git commit -m "Deploying to ${BRANCH} from ${BASE_BRANCH:-master} ${GITHUB_SHA}" --quiet && \
-git push $REPOSITORY_PATH `git subtree split --prefix $FOLDER ${BASE_BRANCH:-master}`:$BRANCH --force && \
-
-echo Deployment succesful!
-
-echo 404 hack...
-sleep 10
-mkdir tmp
-cd tmp
-git clone "$REPOSITORY_PATH"
-cd `basename "$GITHUB_REPOSITORY"`
-echo empty > .nojekyll
-git add .nojekyll
-git commit -m -
-git push "$REPOSITORY_PATH" master
-echo OK. Cleanup...
-cd ../..
-rm -r tmp
+git commit -m "Deploying to ${BRANCH} from ${BASE_BRANCH:-master} ${GITHUB_SHA}" --quiet
+git push $REPOSITORY_PATH `git subtree split --prefix $FOLDER ${BASE_BRANCH:-master}`:$BRANCH --force
