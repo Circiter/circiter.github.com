713f4d180a20b87a9c7a1c73ecf613f7a4e67fb2
Circiter <xcirciter@gmail.com>
2020-01-31 06:35:35 +0600

    _

--- .github/actions/build-blog-action/build.sh
+++ .github/actions/build-blog-action/build.sh
@@ -4,6 +4,8 @@ bundle install
 
 bundle exec jekyll build --trace
 
+latex --version
+
 cd "./$FOLDER"
 #echo > .nojekyll
 if [ -e index.html ]; then mv index.html _.html; fi
@@ -13,70 +15,25 @@ if [ "x$CNAME" != x ]; then
 fi
 cd ..
 
-# Installs Git and jq.
-#apt-get update
-#apt-get install -y git
-#apt-get install -y jq
+COMMIT_EMAIL="${GITHUB_ACTOR:-github-pages-deploy-action}@users.noreply.github.com"
 
-# Gets the commit email/name if it exists in the push event payload.
-#COMMIT_EMAIL=`jq '.pusher.email' ${GITHUB_EVENT_PATH}`
-#COMMIT_NAME=`jq '.pusher.name' ${GITHUB_EVENT_PATH}`
+COMMIT_NAME="${GITHUB_ACTOR:-GitHub Pages Deploy Action}"
 
-# If the commit email/name is not found in the event payload then it falls back to the actor.
-#if [ -z "$COMMIT_EMAIL" ]
-#then
-  COMMIT_EMAIL="${GITHUB_ACTOR:-github-pages-deploy-action}@users.noreply.github.com"
-#fi
-
-#if [ -z "$COMMIT_NAME" ]
-#then
-  COMMIT_NAME="${GITHUB_ACTOR:-GitHub Pages Deploy Action}"
-#fi
-
-# Configures Git.
-#git init
 git config --global user.email "${COMMIT_EMAIL}"
 git config --global user.name "${COMMIT_NAME}"
 
-## Initializes the repository path using the access token.
 REPOSITORY_PATH="https://${ACCESS_TOKEN:-"x-access-token:$GITHUB_TOKEN"}@github.com/${GITHUB_REPOSITORY}.git"
 
-#git checkout "${BASE_BRANCH:-master}"
-
-#rm -r .git
-#git clone "$REPOSITORY_PATH"
-#cd `basename "$GITHUB_REPOSITORY"`
-#git checkout "$BRANCH"
-#for i in `ls "../$FOLDER/"`; do
-#    cp -r "../$FOLDER/$i" .
-#done
-
-#############################
-#echo moving files...
-#mv $FOLDER .$FOLDER
-#rm -r * .github
-#mv .$FOLDER/* .
-#rm -r .$FOLDER
-#git add --all --force
-#git commit --quiet --allow-empty -m -
-#echo pushing...
-#git push --force "$REPOSITORY_PATH" "$BRANCH"
-#############################
-
 rm -r .git
-git clone $REPOSITORY_PATH
-cd `basename "$GITHUB_REPOSITORY"`
-git checkout $BRANCH
-[ `ls . | wc -l` = 0 ] || rm -r *
-[ -e .nojekyll ] && rm .nojekyll
+#git clone $REPOSITORY_PATH
+#cd `basename "$GITHUB_REPOSITORY"`
+mkdir result
+cd result
+git init
+#git checkout $BRANCH
+#[ `ls . | wc -l` = 0 ] || rm -r *
+#[ -e .nojekyll ] && rm .nojekyll
 mv ../$FOLDER/* .
 git add --all --force
 git commit --quiet --allow-empty -m -
 git push --force "$REPOSITORY_PATH" $BRANCH
-
-# Commits the data to Github.
-#git add -f $FOLDER
-#git commit --quiet --allow-empty -m -
-#git push --force "$REPOSITORY_PATH" $BRANCH
-
-#git push $REPOSITORY_PATH `git subtree split --prefix $FOLDER ${BASE_BRANCH:-master}`:$BRANCH --force
