1e5dcb0a01724cf173a5e470fc4eeb95b1e1a0c2
Circiter <xcirciter@gmail.com>
2020-05-01 03:18:00 +0600

    -

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -6,7 +6,9 @@
 module Jekyll
     class TagPageGenerator < Generator
         def generate(site)
-            tags=site.blog_posts.docs.flat_map{|post| post.data['tags']||[]}.to_set
+            posts=site.collections["blog_posts"]
+            #tags=site.posts...
+            tags=posts.docs.flat_map{|post| post.data["tags"]||[]}.to_set
             tags.each do |tag|
                 site.pages << TagPage.new(site, site.source, tag)
             end
@@ -21,7 +23,7 @@ module Jekyll
             @name='index.html'
 
             self.process(@name)
-            self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+            self.read_yaml(File.join(base, "_layouts"), "tag.html")
             self.data['tag']=tag
             self.data['title']="Tag: #{tag}"
             self.data['permalink']="/tag/#{tag}"
