5dbd36d3a64a1296b6f8a2d1c157ef5156c3147d
Circiter <xcirciter@gmail.com>
2020-05-07 06:16:01 +0600

    -

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -25,7 +25,9 @@ module Jekyll
             self.process(@name)
             self.read_yaml(File.join(base, "_layouts"), "tag.html")
             self.data['tag']=tag
-            self.data['permalink']="/tag/#{tag}" # FIXME: Try =@dir instead.
+            self.data['title']=self.data['title']+"#{tag}";
+            #self.data['permalink']="/tag/#{tag}"
+            self.data['permalink']=@dir
         end
     end
 end
