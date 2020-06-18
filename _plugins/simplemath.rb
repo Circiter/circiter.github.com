5328fcc78ce2a8d3a4ac6b3fc236f53ea82ff1b7
Circiter <xcirciter@gmail.com>
2020-06-19 00:24:58 +0600

    -

--- _plugins/simplemath.rb
+++ _plugins/simplemath.rb
@@ -1,9 +1,134 @@
+# Some parts of this code is taken from github.com/fgalindo/jekyll-liquid-latex-plugin
+# but with major rewrite [and, in fact, with considerable downgrade in the functionality].
+
 require "kramdown/converter"
 require "fileutils"
 require "digest"
 require "erb"
 
-# FIXME: module Kramdown::Converter::MathEngine::SimpleMath
+def render_latex(formula, is_formula, inline, site, converter)
+    directory="eq"
+    if !File.exists?(directory)
+        FileUtils.mkdir_p(directory)
+    end
+
+    latex_source="\\documentclass[10pt]{article}\n"
+    latex_source<<"\\usepackage[utf8]{inputenc}\n"
+    latex_source<<"\\usepackage[T2A,T1]{fontenc}\n"
+    latex_source<<"\\usepackage{amsmath,amsfonts,amssymb,color,xcolor}\n"
+    latex_source<<"\\usepackage[english, russian]{babel}\n"
+    latex_source<<"\\usepackage{type1cm}\n"
+    latex_source<<"\\usepackage{tikz}\n"
+    #latex_source<<"\\usepackage{circuitikz}\n"
+    formula_in_brackets=formula
+    if is_formula
+        latex_source<<"\\newsavebox\\frm\n"
+        latex_source<<"\\sbox\\frm{"
+
+        equation_bracket="$"
+        equation_bracket="$$" unless inline
+        formula_in_brackets=equation_bracket+formula+equation_bracket
+
+        latex_source<<formula_in_brackets
+        latex_source<<"}\n\\newwrite\\frmdims\n"
+        latex_source<<"\\immediate\\openout\\frmdims=dimensions.tmp\n"
+        latex_source<<"\\immediate\\write\\frmdims{depth: \\the\\dp\\frm}\n"
+        latex_source<<"\\immediate\\write\\frmdims{height: \\the\\ht\\frm}\n"
+        latex_source<<"\\immediate\\closeout\\frmdims\n"
+    end
+    latex_source<<"\n\\begin{document}\\pagestyle{empty}\n"
+    if is_formula
+        latex_source<<"\\usebox\\frm"
+    else
+        latex_source<<formula
+    end
+    latex_source<<"\n\\end{document}"
+    filename=Digest::MD5.hexdigest(formula_in_brackets)+".png"
+    full_filename=File.join(directory, filename)
+
+    latex_document=File.new("temp-file.tex", "w")
+    latex_document.puts(latex_source)
+    latex_document.close
+    system("latex -interaction=nonstopmode temp-file.tex >/dev/null 2>&1")
+
+    result="<pre>"+formula_in_brackets+"</pre>"
+    if File.exists?("temp-file.dvi")
+        #system("dvipng -q* -T tight temp-file.dvi -o "+full_filename);
+        system("dvips -E temp-file.dvi -o temp-file.eps >/dev/null 2>&1");
+        system("convert -density 120 temp-file.eps "+full_filename+" >/dev/null 2>&1")
+        if File.exists?(full_filename)
+            #system("convert "+full_filename+"-fuzz 2% -transparent white "+full_filename)
+            static_file=Jekyll::StaticFile.new(site, site.source, directory, filename)
+            #Jekyll::Site::register_file(static_file.path) # FIXME.
+            site.static_files<<static_file
+
+            style=""
+            if is_formula
+                depth_pt="0pt"
+                height_pt="10pt"
+                IO.foreach("dimensions.tmp") do |line|
+                    if line =~ /^([a-z]*):\s+(\d*\.?\d+)[a-z]*$/
+                        if $1 == "depth"
+                            depth_pt=$2
+                        elsif $1 == "height"
+                            height_pt=$2
+                        end
+                    end
+                end
+                height_pt_float=height_pt.to_f
+                depth_pt_float=depth_pt.to_f
+
+                # For some reason the depth obtained from the latex
+                # does not give a correct vertical position for a
+                # image on an html-page. But nevertheless we can use
+                # the proportionality between actual size of the image
+                # and the reported dimensions (including the depth) to
+                # convert from pt to px.
+
+                # Try to use ImageMagick's identify to get the height in pixels.
+                system("identify -ping -format %h "+full_filename+" > height.tmp")
+                height_pixels=File.read("height.tmp");
+
+                conversion_factor=(height_pixels.to_f)/height_pt_float
+                depth_pixels=(depth_pt_float*conversion_factor).round.to_i
+
+                depth=depth_pixels.to_s
+
+                #style="margin-bottom: -"+depth+"px;"
+                style="height: "+height_pixels+"px; vertical-align: -"+depth+"px;";
+            end
+            #title=CGI.escape(formula)
+            #title=ERB::Util.url_encode(formula)
+            title=ERB::Util.html_escape(formula) # FIXME.
+            full_filename="/"+full_filename;
+            if is_formula #&&converter!=null
+                if inline
+                    result=converter.format_as_span_html("img",
+                        {"src"=>full_filename, "border"=>0,
+                        "class=>"inline", "style"=>style}, "");
+                    puts("Debug: html generated: <code>"+result+"</code>");
+                else
+                    result=converter.format_as_block_html("img",
+                        {"src"=>full_filename, "border"=>0,
+                        "class=>"inline", "style"=>style}, "", 0);
+                end
+            else
+                result="<img src=\""+full_filename+"\" border=\"0\" class=\"inline\"/>"
+            end
+        else
+            puts "png file does not exist (for formula "+formula+")"
+        end
+    else
+        puts "dvi file was not generated (for formula "+formula+")"
+    end
+
+    Dir.glob("temp-file.*").each do |f|
+        File.delete(f)
+    end
+
+    result
+end
+
 module Kramdown
     module Converter
         module MathEngine
@@ -13,129 +138,15 @@ module Kramdown
                     @@my_site=site
                 end
 
-                @@my_generated_files=[]
-                def self.generated_files
-                    @@my_generated_files
-                end
-
                 def self.call(converter, element, options)
                     display_mode=element.options[:category]
                     formula=element.value
-
-                    directory="eq"
-                    if !File.exists?(directory)
-                        FileUtils.mkdir_p(directory)
-                    end
-
-                    #puts "generating tex document for formula: "+formula
-                    latex_source="\\nonstopmode\n"
-                    latex_source<<"\\documentclass[10pt]{article}\n"
-                    latex_source<<"\\usepackage[utf8]{inputenc}\n"
-                    latex_source<<"\\usepackage[T2A,T1]{fontenc}\n"
-                    latex_source<<"\\usepackage{amsmath,amsfonts,amssymb,color,xcolor}\n"
-                    latex_source<<"\\usepackage[english, russian]{babel}\n"
-                    latex_source<<"\\usepackage{type1cm}\n"
-                    latex_source<<"\\newsavebox\\frm\n"
-                    latex_source<<"\\sbox\\frm{"
-                    #equation_bracket=(display_mode==:block)?"$$":"$"
-                    equation_bracket="$"
-                    #puts("equation display mode is "+display_mode.to_s)
+                    inline=true
                     if display_mode==:block
-                        equation_bracket="$$"
-                    end
-                    formula_in_brackets=equation_bracket+formula+equation_bracket
-                    #puts("processing a formula "+formula_in_brackets)
-                    latex_source<<formula_in_brackets
-                    latex_source<<"}\n\\newwrite\\frmdims\n"
-                    latex_source<<"\\immediate\\openout\\frmdims=dimensions.tmp\n"
-                    latex_source<<"\\immediate\\write\\frmdims{depth: \\the\\dp\\frm}\n"
-                    latex_source<<"\\immediate\\write\\frmdims{height: \\the\\ht\\frm}\n"
-                    latex_source<<"\\immediate\\closeout\\frmdims\n"
-                    latex_source<<"\n\\begin{document}\\pagestyle{empty}\\usebox\\frm\\end{document}"
-                    filename=Digest::MD5.hexdigest(formula_in_brackets)+".png"
-                    full_filename=File.join(directory, filename)
-
-                    latex_document=File.new("temp-file.tex", "w")
-                    latex_document.puts(latex_source)
-                    latex_document.close
-                    #puts "trying to compile latex document..."
-                    system("latex -interaction=nonstopmode temp-file.tex >/dev/null 2>&1")
-
-                    result=formula_in_brackets
-                    if File.exists?("temp-file.dvi")
-                        #puts "converting dvi to png..."
-                        #system("dvipng -q* -T tight temp-file.dvi -o "+full_filename);
-                        system("dvips -E temp-file.dvi -o temp-file.eps >/dev/null 2>&1");
-                        system("convert -density 120 temp-file.eps "+full_filename+" >/dev/null 2>&1")
-                        if File.exists?(full_filename)
-                            #system("convert "+full_filename+"-fuzz 2% -transparent white "+full_filename)
-                            #convert test.png -background 'rgba(0,0,0,0)' test1.png
-                            #site=Jekyll.sites[0]
-                            site=@@my_site
-                            #puts("site.source="+site.source)
-                            static_file=Jekyll::StaticFile.new(site, site.source, directory, filename)
-                            @@my_generated_files<<static_file
-                            site.static_files<<static_file
-                            #puts "finalizing"
-
-                            depth_pt="0pt"
-                            height_pt="10pt"
-                            IO.foreach("dimensions.tmp") do |line|
-                                if line =~ /^([a-z]*):\s+(\d*\.?\d+)[a-z]*$/
-                                    if $1 == "depth"
-                                        depth_pt=$2
-                                    elsif $1 == "height"
-                                        height_pt=$2
-                                    end
-                                end
-                            end
-                            height_pt_float=height_pt.to_f
-                            depth_pt_float=depth_pt.to_f
-
-                            # For some reason the depth obtained from the latex
-                            # does not give a correct vertical position for a
-                            # image on an html-page. But nevertheless we can use
-                            # the proportionality between actual size of the image
-                            # and the reported dimensions (including the depth) to
-                            # convert from pt to px.
-
-                            # Try to use ImageMagick's identify to get the height in pixels.
-                            system("identify -ping -format %h "+full_filename+" > height.tmp")
-                            height_pixels=File.read("height.tmp");
-
-                            conversion_factor=(height_pixels.to_f)/height_pt_float
-                            depth_pixels=(depth_pt_float*conversion_factor).round.to_i
-
-                            depth=depth_pixels.to_s
-
-                            #style="margin-bottom: -"+depth+"px;"
-                            style="height: "+height_pixels+"px; vertical-align: -"+depth+"px;";
-                            #result="<img src=\"/"+full_filename+"\" title=\""+formula+"\" style=\""+style+"\" class=\"inline\" />"
-                            #title=CGI.escape(formula) # FIXME.
-                            #title=ERB::Util.url_encode(formula) # FIXME.
-                            title=ERB::Util.html_escape(formula) # FIXME.
-                            if display_mode==:block
-                                result=converter.format_as_block_html("img",
-                                    {"src"=>"/"+full_filename, "title"=>title, "border"=>0,
-                                    "class"=>"inline", "style"=>style}, "", 0);
-                            else
-                                result=converter.format_as_span_html("img",
-                                    {"src"=>"/"+full_filename, "title"=>title, "border"=>0,
-                                    "class"=>"inline", "style"=>style}, "");
-                            end
-                            #puts "ok"
-                        else
-                            puts "png file does not exist"
-                        end
-                    else
-                        puts "dvi file was not generated"
+                        inline=false
                     end
 
-                    Dir.glob("temp-file.*").each do |f|
-                        File.delete(f)
-                    end
-
-                    result
+                    render_latex(formula, true, inline, @@my_site, converter)
                 end
             end
         end
@@ -145,17 +156,22 @@ end
 Kramdown::Converter.add_math_engine(:simplemath, Kramdown::Converter::MathEngine::SimpleMath)
 
 class Jekyll::Site
+    def initialize()
+        @xfiles=[]
+    end
+
+    def register_file(filename)
+        @xfiles<<filename
+    end
+
     alias :super_write :write
+
     def write
-        super_write # FIXME: Why super() doesn't work?
+        super_write
         source_files=[]
+        #source_files=@xfiles
         puts "generated files:"
-        Kramdown::Converter::MathEngine::SimpleMath::generated_files.each do |f|
-            puts(f.path)
-            source_files<<f.path
-        end
-        puts "files in eq/:"
-        Dir.glob("eq/*.png").each do |f|
+        source_files.each do |f|
             puts(f)
         end
         to_remove=Dir.glob("eq/*.png")-source_files # FIXME.
@@ -164,7 +180,7 @@ class Jekyll::Site
             puts(f)
             if File.exists?(f)
                 puts("removing "+f)
-                File.unlink(f)
+                #File.unlink(f)
             end
         end
     end
@@ -182,6 +198,7 @@ def fix_math(content)
     return mathfix.fixup()
 end
 
+# FIXME: How it will interoperate with a "latex" tag defined below?
 class MathFix
     def initialize(content)
         @content=content
@@ -258,3 +275,24 @@ Jekyll::Hooks.register(:blog_posts, :pre_render) do |target, payload|
         target.content=fix_math(target.content)
     end
 end
+
+module Jekyll
+    module Tags
+        class LatexBlock < Liquid::Block
+            include Liquid::StandardFilters
+
+            def initialize(tag_name, text, tokens)
+                super
+            end
+
+            def render(context)
+                latex_source=super
+                site=context.registers[:site]
+                render_latex(latex_source, false, false, site, null)
+            end
+
+        end
+    end
+end
+
+Liquid::Template.register_tag("latex", Jekyll::Tags::LatexBlock)
